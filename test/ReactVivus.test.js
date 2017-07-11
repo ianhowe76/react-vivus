661a21c33dc94a7211b9fe94b1cd9b97d03629f6
Ian Howe <ian.howe@loanmarket.com.au>
2017-07-12 08:52:40 +1000

    Test that vivus appears

--- test/ReactVivus.test.js
+++ test/ReactVivus.test.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 
-import renderer from 'react-test-renderer';
 import { mount } from 'enzyme';
+import sinon from 'sinon';
 
 import ReactVivus from '../src/ReactVivus';
 
 const TEST_SVG = '../testData/test-animation.svg';
 
 let rvWrapper;
+let clock;
 
 describe('<ReactVivus />', () => {
   describe('when only svg is specified', () => {
     beforeAll(() => {
+      clock = sinon.useFakeTimers(Date.now());
       rvWrapper = mount(
         <ReactVivus
           svg={TEST_SVG}
-        />
+        />,
+        { attachTo: document.body },
       );
+    });
+
+    afterAll(() => {
+      clock.restore();
+      rvWrapper.detach();
     });
 
     test('component exists', () => {
@@ -31,6 +39,19 @@ describe('<ReactVivus />', () => {
       expect(rvWrapper.prop('animTiming')).toBe(ReactVivus.defaultProps.animTiming);
       expect(rvWrapper.prop('onAnimationFinished')).toBe(ReactVivus.defaultProps.onAnimationFinished);
       expect(rvWrapper.prop('name')).not.toBeNull();
+    });
+
+    test('has created a div container', () => {
+      expect(rvWrapper.find('div').length).toBe(1);
+    });
+
+    test('has not yet created a vivus', () => {
+      expect(rvWrapper.instance().vivus).toBeUndefined();
+    });
+
+    test('has created a vivus after the delay', () => {
+      clock.tick(ReactVivus.defaultProps.delayMs + 10);
+      expect(rvWrapper.instance().vivus).not.toBeUndefined();
     });
   });
 });
