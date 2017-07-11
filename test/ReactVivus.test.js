d7a32e1207be5be524a729fbae2c4f6b18a8ab5d
Ian Howe <ian.howe@loanmarket.com.au>
2017-07-11 16:44:38 +1000

    Test default props

--- test/ReactVivus.test.js
+++ test/ReactVivus.test.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import ReactVivus from '../src/ReactVivus';
 
 const TEST_SVG = '../testData/test-animation.svg';
 
-test('<ReactVivus />', () => {
-  const wrapper = shallow(
-    <ReactVivus
-      svg={TEST_SVG}
-    />
-  );
+let rvWrapper;
 
-  expect(wrapper).not.toBeNull();
+describe('<ReactVivus />', () => {
+  describe('when only svg is specified', () => {
+    beforeAll(() => {
+      rvWrapper = mount(
+        <ReactVivus
+          svg={TEST_SVG}
+        />
+      );
+    });
+
+    test('component exists', () => {
+      expect(rvWrapper).not.toBeNull();
+    });
+
+    test('has default props', () => {
+      expect(rvWrapper.prop('duration')).toBe(ReactVivus.defaultProps.duration);
+      expect(rvWrapper.prop('delayMs')).toBe(ReactVivus.defaultProps.delayMs);
+      expect(rvWrapper.prop('type')).toBe(ReactVivus.defaultProps.type);
+      expect(rvWrapper.prop('pathTiming')).toBe(ReactVivus.defaultProps.pathTiming);
+      expect(rvWrapper.prop('animTiming')).toBe(ReactVivus.defaultProps.animTiming);
+      expect(rvWrapper.prop('onAnimationFinished')).toBe(ReactVivus.defaultProps.onAnimationFinished);
+      expect(rvWrapper.prop('name')).not.toBeNull();
+    });
+  });
 });
 
