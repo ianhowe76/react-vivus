0bec317d201f9c83646342b3e93822ae0aea5937
Ian Howe <ian@vbizapp.com>
2018-02-06 20:41:01 +1100

    Add image to demo

--- demo/app.js
+++ demo/app.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactVivus from '../src/ReactVivus';
 
-class Demo extends Component {
-  render() {
-    return (
-      <div style={{ height: '400px', width: '400px', backgroundColor: 'green' }}>
-        <ReactVivus name="test" svg="test-animation.svg" color="orange" delayMs={1000}/>
-      </div>
-    )
-  }
-}
+const TestAnimation = 'images/test-animation.svg';
+
+const Demo = () => {
+  console.log(TestAnimation);
+  return (
+    <div style={{ height: '400px', width: '400px', backgroundColor: 'green' }}>
+      <ReactVivus name="test" svg={TestAnimation} color="orange" type="oneByOne" delayMs={1000} />
+    </div>
+  );
+};
 
 ReactDOM.render(<Demo />, document.getElementById('root'));
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,5 @@ const getConfig = require('hjs-webpack');
 module.exports = getConfig({
   in: 'demo/app.js',
   out: 'public',
-  clearBeforeBuild: true,
+  clearBeforeBuild: '!(images)',
 });
